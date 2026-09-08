a55e5a10cd639ff0811942b29b1afe7c85b6fcf5
keerthi-180205 <keerthin180205@gmail.com>
2026-09-09 00:19:32 +0530

    dvc s3 pushed

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -8,6 +8,7 @@ import logging
 import yaml
 from dvclive import Live
 
+
 # Ensure the "logs" directory exists
 log_dir = 'logs'
 os.makedirs(log_dir, exist_ok=True)
@@ -29,6 +30,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 
 def load_model(file_path:str):
@@ -91,6 +109,7 @@ def save_matrics(metrics : dict, file_path : str):
         raise
 def main():
     try :
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
 
@@ -98,6 +117,14 @@ def main():
         y_test = test_data.iloc[:,-1].values
 
         metrics = evaluate_model(clf,X_test, y_test)
+
+        # Experiment tracking using dvclive
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+
+            live.log_params(params)
 
         save_matrics(metrics, 'reports/metrics.json')
     except Exception as e:
